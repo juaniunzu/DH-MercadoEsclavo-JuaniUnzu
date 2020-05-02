2b8100a465eff039353ad09122041f22a764677f
Juani Unzurrunzaga <juanunzu@gmail.com>
2020-05-01 23:52:43 -0300

    clase articulo fragment terminada

--- app/src/main/java/com/example/dh_mercadoesclavo/ArticuloFragment.java
+++ app/src/main/java/com/example/dh_mercadoesclavo/ArticuloFragment.java
@@ -3,10 +3,14 @@ package com.example.dh_mercadoesclavo;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.List;
 
 
 /**
@@ -22,7 +26,25 @@ public class ArticuloFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_articulo, container, false);
+
+        //se infla el xml del fragment a View
+        View view = inflater.inflate(R.layout.fragment_articulo, container, false);
+
+        //se asocia el recyclerView del fragment con el elemento xml. Se hace como variable local
+        //pero también se puede poner como atributo del fragment.
+        RecyclerView fragmentArticuloRecyclerView = view.findViewById(R.id.fragmentArticuloRecyclerView);
+
+        //creo lista que sera parametro en la construccion del adapter
+        List<Articulo> listaDeArticulos = ProveedorDeArticulos.getArticulos();
+        //creo adapter
+        ArticuloAdapter articuloAdapter = new ArticuloAdapter(listaDeArticulos);
+        //creo layout manager
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(view.getContext(), RecyclerView.VERTICAL, false);
+
+        //seteo adapter y layout manager al recyclerView
+        fragmentArticuloRecyclerView.setLayoutManager(linearLayoutManager);
+        fragmentArticuloRecyclerView.setAdapter(articuloAdapter);
+        
+        return view;
     }
 }
