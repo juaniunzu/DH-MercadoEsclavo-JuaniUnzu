26dc220f8175e9736231af0c3233f4908d704548
Juani Unzurrunzaga <juanunzu@gmail.com>
2020-05-01 23:18:03 -0300

    layouts terminados

---
+++ app/src/main/java/com/example/dh_mercadoesclavo/ArticuloFragment.java
@@ -0,0 +1,28 @@
+package com.example.dh_mercadoesclavo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ArticuloFragment extends Fragment {
+
+    public ArticuloFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_articulo, container, false);
+    }
+}
