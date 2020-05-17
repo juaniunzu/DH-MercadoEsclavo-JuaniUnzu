6fd708c11dad6999cbdb84b33ca26960318c1694
Juani Unzurrunzaga <juanunzu@gmail.com>
2020-05-16 22:13:52 -0300

    se agrega about us fragment y navigation view

--- app/src/main/java/com/example/dh_mercadoesclavo/view/MainActivity.java
+++ app/src/main/java/com/example/dh_mercadoesclavo/view/MainActivity.java
@@ -1,23 +1,37 @@
 package com.example.dh_mercadoesclavo.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.dh_mercadoesclavo.R;
 import com.example.dh_mercadoesclavo.model.Articulo;
 import com.example.dh_mercadoesclavo.view.ArticuloFragment;
 import com.example.dh_mercadoesclavo.view.DetailFragment;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity implements ArticuloFragment.ArticuloFragmentListener {
+public class MainActivity extends AppCompatActivity implements ArticuloFragment.ArticuloFragmentListener, NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout activityMainDrawerLayout;
+    private NavigationView activityMainNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activityMainDrawerLayout = findViewById(R.id.activityMainDrawerLayout);
+        activityMainNavigationView = findViewById(R.id.activityMainNavigationView);
+
+        activityMainNavigationView.setNavigationItemSelectedListener(this);
+
 
         //creo fragment y lo pego en el main
         ArticuloFragment articuloFragment = new ArticuloFragment();
@@ -40,5 +54,17 @@ public class MainActivity extends AppCompatActivity implements ArticuloFragment.
         DetailFragment fragment = new DetailFragment();
         fragment.setArguments(mainADetalle);
         pegarFragment(fragment);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.navigationMenuAboutUs:
+                pegarFragment(new AboutUsFragment());
+                activityMainDrawerLayout.closeDrawers();
+                break;
+        }
+        return false;
     }
 }
